3f8d06a5f018f3393eb5680d02679e879313a4d4
Tobias <tobi.l@posteo.at>
2021-03-05 14:04:04 +0100

    test: add unit tests for getKeyFromRouter

--- lib/key.test.ts
+++ lib/key.test.ts
@@ -8,3 +8,11 @@ it("should return key from URL", () => {
   const testedKey = getKeyFromRouter({ asPath: urlPath });
   expect(testedKey).toBe(key);
 });
+
+it("should return empty string from URL if no key there", () => {
+  const urlPath =
+    "/decrypt/VTJGc2RHVmtYMStuVExkTzRNR0xpS2FySmtRa0lrdDZDbno0NXo1ZDZiTT0=";
+
+  const testedKey = getKeyFromRouter({ asPath: urlPath });
+  expect(testedKey).toBe("");
+});
